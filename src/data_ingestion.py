913b56f3af33cebfabd2732426814b1200da89fe
Pranay Gundu <pranaygundu423@gmail.com>
2025-07-18 04:10:51 +0530

    add second experiment

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -6,9 +6,9 @@ df = pd.read_csv('https://raw.githubusercontent.com/araj2/customer-database/mast
 print(df.shape)
 df = df.iloc[:, 3:]
 
-df = df[df['Length of Membership'] > 1]
+df = df[df['Length of Membership'] > 3]
 
-df.drop(columns=['Avg. Session Length'],inplace=True)
+#df.drop(columns=['Avg. Session Length'],inplace=True)
 print(df.shape)
 
 df.to_csv(os.path.join('data','customer.csv'))
